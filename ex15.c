59b9df810abb220efa288a556936d2f00ce53630
Diego Armando <Armandilho1939@gmail.com>
2020-05-16 23:31:24 -0300

    more code

--- ex15.c
+++ ex15.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
-int main() {
-  int n, j, m, cont;
-  printf("Entre com um número positivo n: \n");
+int main()
+{
+  int n, j, m, cont = 0, i = 0;
+  printf("Entre com um numero positivo n: \n");
   scanf("%d", &n);
-  printf("Entre com um número positivo j: \n");
+  printf("Entre com um numero positivo j: \n");
   scanf("%d", &j);
-  printf("Entre com um número positivo m: \n");
+  printf("Entre com um numero positivo m: \n");
   scanf("%d", &m);
-  for () {
-    if ( % m == j % m) {
-
+  while (n > cont)
+  {
+    if ((i % m) == (j % m))
+    {
+      printf("%d\n", i);
+      cont++;
     }
+    i++;
   }
   return 0;
 }
